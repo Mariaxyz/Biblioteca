80f8f551cd1d3ff6011399f6b6dfeab11c145cce
Mariaxyz <66808274+Mariaxyz@users.noreply.github.com>
2020-06-11 20:04:29 -0500

    Update Material.h

--- Material.h
+++ Material.h
@@ -5,3 +5,18 @@
 usando std :: string;
 clase  Material {
 privado:
+  int codigo;
+  string autor;
+  string titulo;
+  int anio;
+  bool estado;
+  
+public:
+  Material();
+  Material(string codigo, string autor, string titulo, string anio, string estado)
+  int getCodigo();
+  string getAutor();
+  string getTitulo();
+  int getAnio();
+  bool getEstado();
+};
